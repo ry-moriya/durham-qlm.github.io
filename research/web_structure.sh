eef999947e045a4767aaffb103f0698299276c26
xsection <s.a.wrathmall@durham.ac.uk>
2023-06-04 15:18:22 +0100

    structure june 4

---
+++ research/web_structure.sh
@@ -0,0 +1,116 @@
+mkdir -p qfali/beyond/img
+mkdir -p coldmol/groundstate/img
+mkdir -p coldmol/polar/img
+mkdir -p coldmol/fermionic/img
+mkdir -p coldmol/tweezers/img
+mkdir -p coldmol/microscope/img
+mkdir -p coldmol/potentials/img
+mkdir -p coldmol/scattering/img
+mkdir -p coldmol/collisions/img
+mkdir -p coldmol/cafcool/img
+mkdir -p quantcomp/devices/img
+mkdir -p quantcomp/continuous/img
+mkdir -p quantcomp/errorcorrect/img
+mkdir -p quantcomp/hybrid/img
+mkdir -p quantcomp/Annealing/img
+mkdir -p rydberg/photonics/img
+mkdir -p rydberg/nanotomics/img
+mkdir -p rydberg/strontium/img
+mkdir -p rydberg/excitons/img
+mkdir -p tech/breath/img
+mkdir -p tech/structured/img
+mkdir -p tech/thz/img
+mkdir -p vapours/optics/img
+mkdir -p vapours/solar/img
+mkdir -p vapours/magnets/img
+mkdir -p vapours/elecsus/img
+mkdir -p vapours/thermal_ryd/img
+mkdir -p vapours/nanocell/img
+mkdir -p vapours/calcs/img
+touch qfali/beyond.md
+touch coldmol/groundstate.md
+touch coldmol/polar.md
+touch coldmol/fermionic.md
+touch coldmol/tweezers.md
+touch coldmol/microscope.md
+touch coldmol/potentials.md
+touch coldmol/scattering.md
+touch coldmol/collisions.md
+touch coldmol/cafcool.md
+touch quantcomp/devices.md
+touch quantcomp/continuous.md
+touch quantcomp/errorcorrect.md
+touch quantcomp/hybrid.md
+touch quantcomp/Annealing.md
+touch rydberg/photonics.md
+touch rydberg/nanotomics.md
+touch rydberg/strontium.md
+touch rydberg/excitons.md
+touch tech/breath.md
+touch tech/structured.md
+touch tech/thz.md
+touch vapours/optics.md
+touch vapours/solar.md
+touch vapours/magnets.md
+touch vapours/elecsus.md
+touch vapours/thermal_ryd.md
+touch vapours/nanocell.md
+touch vapours/calcs.md
+touch qfali/beyond/.placeholder
+touch coldmol/groundstate/.placeholder
+touch coldmol/polar/.placeholder
+touch coldmol/fermionic/.placeholder
+touch coldmol/tweezers/.placeholder
+touch coldmol/microscope/.placeholder
+touch coldmol/potentials/.placeholder
+touch coldmol/scattering/.placeholder
+touch coldmol/collisions/.placeholder
+touch coldmol/cafcool/.placeholder
+touch quantcomp/devices/.placeholder
+touch quantcomp/continuous/.placeholder
+touch quantcomp/errorcorrect/.placeholder
+touch quantcomp/hybrid/.placeholder
+touch quantcomp/Annealing/.placeholder
+touch rydberg/photonics/.placeholder
+touch rydberg/nanotomics/.placeholder
+touch rydberg/strontium/.placeholder
+touch rydberg/excitons/.placeholder
+touch tech/breath/.placeholder
+touch tech/structured/.placeholder
+touch tech/thz/.placeholder
+touch vapours/optics/.placeholder
+touch vapours/solar/.placeholder
+touch vapours/magnets/.placeholder
+touch vapours/elecsus/.placeholder
+touch vapours/thermal_ryd/.placeholder
+touch vapours/nanocell/.placeholder
+touch vapours/calcs/.placeholder
+touch qfali/beyond/img/.placeholder
+touch coldmol/groundstate/img/.placeholder
+touch coldmol/polar/img/.placeholder
+touch coldmol/fermionic/img/.placeholder
+touch coldmol/tweezers/img/.placeholder
+touch coldmol/microscope/img/.placeholder
+touch coldmol/potentials/img/.placeholder
+touch coldmol/scattering/img/.placeholder
+touch coldmol/collisions/img/.placeholder
+touch coldmol/cafcool/img/.placeholder
+touch quantcomp/devices/img/.placeholder
+touch quantcomp/continuous/img/.placeholder
+touch quantcomp/errorcorrect/img/.placeholder
+touch quantcomp/hybrid/img/.placeholder
+touch quantcomp/Annealing/img/.placeholder
+touch rydberg/photonics/img/.placeholder
+touch rydberg/nanotomics/img/.placeholder
+touch rydberg/strontium/img/.placeholder
+touch rydberg/excitons/img/.placeholder
+touch tech/breath/img/.placeholder
+touch tech/structured/img/.placeholder
+touch tech/thz/img/.placeholder
+touch vapours/optics/img/.placeholder
+touch vapours/solar/img/.placeholder
+touch vapours/magnets/img/.placeholder
+touch vapours/elecsus/img/.placeholder
+touch vapours/thermal_ryd/img/.placeholder
+touch vapours/nanocell/img/.placeholder
+touch vapours/calcs/img/.placeholder
